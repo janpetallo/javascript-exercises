526dffff5ff7fa6b79c3d6e9bc8db32fa316e276
JanP <101074771+janpetallo@users.noreply.github.com>
2024-01-02 19:58:55 -0700

    Completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    
+    if(numberOfTimes < 0){
+        return 'ERROR';
+    }
 
+    let stringToReturn = '';
+    for(let i = 0; i < numberOfTimes; i++){
+        stringToReturn += stringToRepeat;
+    }
+    return stringToReturn;
 };
 
 // Do not edit below this line
