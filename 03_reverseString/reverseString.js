be6e9484c54fc24e10f98811da7d25b0d2bd7fa3
JanP <101074771+janpetallo@users.noreply.github.com>
2024-01-02 20:22:48 -0700

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let reversedString = '';
+    const stringArray = stringToReverse.split("");
+    let i = stringArray.length - 1;
+    while(i >= 0){
+        reversedString += stringArray[i];
+        i--;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
