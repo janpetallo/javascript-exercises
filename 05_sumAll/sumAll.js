4ddd18b9fa428358a9b98d5d7cb4d07100763edf
JanP <101074771+janpetallo@users.noreply.github.com>
2024-01-02 21:54:47 -0700

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if(firstNumber < 0 || secondNumber < 0
+        || typeof(firstNumber) !== typeof(1) || typeof(secondNumber) !== typeof(1)){
+            return 'ERROR'
+        } 
 
+    if(secondNumber < firstNumber){
+        let temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+
+    let sum = 0;
+    for(let i = firstNumber; i <= secondNumber; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yearToDetermine) {
+    if(yearToDetermine % 4 === 0 &&
+        yearToDetermine % 100 === 0 &&
+        yearToDetermine % 400 === 0) {
+            return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
