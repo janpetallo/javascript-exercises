a3f5c496627a627791815ba1c1b980381ef1f2a4
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-23 13:59:30 -0600

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Remove all non-alphanumeric characters and convert to lowercase
+    const cleanedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    // split the cleaned string into an array of characters, 
+    // reverse the array, 
+    // join the array back into a string
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
