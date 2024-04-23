7b222f9852b31f433d9d7b22d2a2cefefe2b4ac7
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-23 14:16:20 -0600

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+// returns the nth number in the fibonacci sequence
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS"; // negative numbers are not allowed
+    if (n == 0) return 0; // 0th fibonacci number is 0
+    if (n == 1) return 1; // 1st fibonacci number is 1
+    let a = 0; // 0th fibonacci number
+    let b = 1; // 1st fibonacci number
+    let c = 1; // 2nd fibonacci number
+    // loop through the fibonacci sequence
+    for (let i = 2; i <= n; i++) {
+        c = a + b; // calculate the next fibonacci number
+        a = b; // update the first number
+        b = c; // update the second number
+    }
+    return c;
 
 };
 
