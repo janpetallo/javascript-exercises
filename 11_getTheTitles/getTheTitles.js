86fac8a9be795816f34003aa581d779b554bf531
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-23 14:24:43 -0600

    completed 11_getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // map() method creates a new array with the results of 
+    // calling a provided function on every element in the calling array.
+    return books.map(book => book.title);
 
 };
 
